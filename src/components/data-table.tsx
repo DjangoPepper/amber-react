4183dedb59cc764b37fc82b4808051ef19a5be92
Lionel Gabaude <lgabaude@treasuryspring.com>
2023-02-13 09:47:53 +0100

    reduce column size for prepa

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -75,6 +75,7 @@ const EditableCell = ({ getValue, row, column, table }: any) => {
             value={value as string}
             onChange={e => setValue(e.target.value)}
             onBlur={onBlur}
+            style={{width: "50px"}}
         />
     )
 };
