4c4e4b47cc974ffdb442cc8940af9067f04ae569
DjangoPepper <fr3d.mobile@free.fr>
2023-07-02 14:35:58 +0200

    essai sur trouverunelettre

--- src/components/SpaceatPos.tsx
+++ src/components/SpaceatPos.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+// import React from "react";
+// import { passiveEventSupported } from "@tanstack/react-table";
 
-interface SpacedAtPositionStringProps {
-  numberastext: string;
+// interface SpacedAtPositionStringProps {
+//   numberastext: string;
 
+// }
+
+function trouverPosition1ereLettre(chaine: string): number {
+  let result: number = -1;                                    // Initialise le résultat à -1 aucune lettre trouvée
+  const chaineEnMajuscules: string = chaine.toUpperCase();    // transforme la chaine en majuscules
+  
+  for (let i: number = 2; i < (chaineEnMajuscules.length-2); i++) {   //Initialise I, boucle de 2 à longeur chaine
+    const code: number = chaineEnMajuscules.charCodeAt(i);        //Transforme le caractere en code
+    if (code >= 65 && code <= 90) {                               //Si code represente une lettre majuscule
+      result = i;                                                 //Récupère la position de la lettre trouvée   
+      break; // Sort de la boucle dès qu'une lettre est trouvée   //break
+    }
+  }
+
+  return result;                                                  // Retourne result
 }
-// function useColumns(): any[] {
+
 function insertSpaceAtPosition(numberastext: string, position: number): string {
   if (position < 0 || position > numberastext.length) {
     throw new Error('Invalid position');
@@ -51,7 +67,18 @@ function definehowmanyspace(originalNumber: string): string {
 }
 
 const SpacedString = (_ref:string) => {
-  const HumanspacedText = definehowmanyspace(_ref);
-  return HumanspacedText;
+  let newpos:number = trouverPosition1ereLettre(_ref);
+  if (newpos === -2) {                                 // pas de lettre trouvée dans la chaine
+    const HumanspacedText = definehowmanyspace(_ref);
+    return HumanspacedText;
+  }
+  else {                                              // lettre trouvée dans la chaine
+    _ref = insertSpaceAtPosition(_ref, newpos)
+    const HumanspacedText:string = insertSpaceAtPosition(_ref, (newpos+2))
+    return HumanspacedText;
+  } 
+  // const HumanspacedText = definehowmanyspace(_ref);
+  // return HumanspacedText;
+    
 }
 export default SpacedString;
