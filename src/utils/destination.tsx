12840aa10f340d451fcabc155104d6f2fa4eac47
DjangoPepper <fr3d.mobile@free.fr>
2023-09-11 15:29:26 +0200

    ajout index a destinations et key lmargeur

--- src/utils/destination.tsx
+++ src/utils/destination.tsx
@@ -11,17 +11,18 @@ export const HEADER: IHeader[] = [
     {key: "prepa", name :"PREPA" },
     {key: "reference", name :"REF" },
     {key: "weight", name :"POIDS" },
+    {key: "width", name :"LARGEUR" },
     {key: "position", name :"POS" },
     {key: "destination", name :"DEST", default: "NE" },
 ]
 
 export const destinations = [
-    {name: "stock", color: "#fff"},
-    {name: "C1", color: "#00c87a"},
-    {name: "C2", color: "#3cbefc"},
-    {name: "C3", color: "#9d64e2"},
-    {name: "C4", color: "#f447d1"},
-    {name: "C5", color: "#fdff5b"},
+    {name: "stock", color: "#fff", index:0},
+    {name: "C1", color: "#00c87a", index:1},
+    {name: "C2", color: "#3cbefc", index:2},
+    {name: "C3", color: "#9d64e2", index:3},
+    {name: "C4", color: "#f447d1", index:4},
+    {name: "C5", color: "#fdff5b", index:5},
 ]
 
 
