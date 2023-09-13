44cb26512dc42a9851d8b1b24bab7efcca44f1fd
DjangoPepper <fr3d.mobile@free.fr>
2023-09-13 13:34:40 +0200

    bof

--- src/stores/data/DataAction.ts
+++ src/stores/data/DataAction.ts
@@ -1,61 +1,72 @@
-import {AnyAction} from "redux";
+import { AnyAction } from "redux";
 
 export default class DataAction {
-	public static IMPORT_DATA = "DataAction.IMPORT_DATA";
-	public static CHANGE_CALE = "DataAction.CHANGE_CALE";
-	public static CHANGE_PREPA = "DataAction.CHANGE_PREPA";
-	public static MOVE_ROW = "DataAction.MOVE_ROW";
-	public static UPDATE_ROW = "DataAction.UPDATE_ROW";
-	public static SAVE = "DataAction.SAVE";
-	public static LOAD = "DataAction.LOAD";
-	public static CLEAR = "DataAction.CLEAR";
-	public static REMOVE = "DataAction.REMOVE";
+  // Définissez les types d'action comme des constantes
+ public static IMPORT_DATA = "DataAction.IMPORT_DATA";
+ public static CHANGE_CALE = "DataAction.CHANGE_CALE";
+ public static CHANGE_PREPA = "DataAction.CHANGE_PREPA";
+ public static MOVE_ROW = "DataAction.MOVE_ROW";
+ public static UPDATE_ROW = "DataAction.UPDATE_ROW";
+ public static SAVE = "DataAction.SAVE";
+ public static LOAD = "DataAction.LOAD";
+ public static CLEAR = "DataAction.CLEAR";
+ public static REMOVE = "DataAction.REMOVE";
+ public static CHANGE_COULEUR = "DataAction.CHANGE_COULEUR";
+ public static CHANGE_ORIGINAL_POS = "DataAction.ORIGINAL_POS";
+ public static DELETE_ROW = "DataAction.DELETE_ROW";
+ public static CHANGE_DESTINATION_COLOR = 'DataAction.CHANGE_DESTINATION_COLOR';
 
-	public static CHANGE_COULEUR = "DataAction.CHANGE_COULEUR";
-	public static CHANGE_ORIGINAL_POS = "DataAction.ORIGINAL_POS";
-	public static DELETE_ROW = "DataAction.DELETE_ROW";
-	
-	public static deleteRow(rank: string): AnyAction {
-		return { type: DataAction.DELETE_ROW, payload: rank };
-	}
+  // Définissez les créateurs d'actions comme des fonctions
+  public static deleteRow(rank: string): AnyAction {
+    return { type: DataAction.DELETE_ROW, payload: rank };
+  }
 
-	public static changeCouleur(cale: any[]): AnyAction {
-		return { type: DataAction.CHANGE_COULEUR, payload: cale };
-	}
-	
-	public static changeOriginalpos(destination: string): AnyAction {
-		return { type: DataAction.CHANGE_ORIGINAL_POS, payload: "stock"};
-	}
-	
-	public static importData(data: any[]): AnyAction {
-		return { type: DataAction.IMPORT_DATA, payload: data };
-	}
+  public static changeCouleur(cale: any[]): AnyAction {
+    return { type: DataAction.CHANGE_COULEUR, payload: cale };
+  }
 
-	public static changeCale(cale: string): AnyAction {
-		return { type: DataAction.CHANGE_CALE, payload: cale };
-	}
+  public static changeOriginalpos(destination: string): AnyAction {
+    return { type: DataAction.CHANGE_ORIGINAL_POS, payload: "stock" };
+  }
 
-	public static changePrepa(prepa: string): AnyAction {
-		return { type: DataAction.CHANGE_PREPA, payload: prepa };
-	}
+  public static importData(data: any[]): AnyAction {
+    return { type: DataAction.IMPORT_DATA, payload: data };
+  }
 
-	public static updateRow(reference: number, columnId: string, value: any) {
-		return {type: DataAction.UPDATE_ROW, payload: {reference, columnId, value}};
-	}
+  public static changeCale(cale: string): AnyAction {
+    return { type: DataAction.CHANGE_CALE, payload: cale };
+  }
 
-	public static moveRow(reference: string): AnyAction {
-		return { type: DataAction.MOVE_ROW, payload: reference };
-	}
+  public static changePrepa(prepa: string): AnyAction {
+    return { type: DataAction.CHANGE_PREPA, payload: prepa };
+  }
 
-	public static save(): AnyAction {
-		return { type: DataAction.SAVE };
-	}
+  public static updateRow(reference: number, columnId: string, value: any) {
+    return { type: DataAction.UPDATE_ROW, payload: { reference, columnId, value } };
+  }
 
-	public static load(data: string): AnyAction {
-		return { type: DataAction.LOAD, payload: JSON.parse(data) };
-	}
+  public static moveRow(reference: string): AnyAction {
+    return { type: DataAction.MOVE_ROW, payload: reference };
+  }
 
-	public static clear(): AnyAction {
-		return { type: DataAction.CLEAR };
-	}
+  public static save(): AnyAction {
+    return { type: DataAction.SAVE };
+  }
+
+  public static load(data: string): AnyAction {
+    return { type: DataAction.LOAD, payload: JSON.parse(data) };
+  }
+
+  public static clear(): AnyAction {
+    return { type: DataAction.CLEAR };
+  }
+
+  // Nouvelle action pour changer la couleur de la destination
+  public static changeDestinationColor(destinationName: string, newColor: string): AnyAction {
+    return {
+      type: DataAction.CHANGE_DESTINATION_COLOR,
+      destinationName,
+      newColor,
+    };
+  }
 }
